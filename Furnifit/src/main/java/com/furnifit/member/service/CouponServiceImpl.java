d77644cf257f22476eea6aabcea4f03eb5d71082
furnifit <dinozo12112@gmail.com>
2017-11-15 11:00:39 +0900

    쿠폰 dao, service, serviceImpl 만드는중

---
+++ Furnifit/src/main/java/com/furnifit/member/service/CouponServiceImpl.java
@@ -0,0 +1,29 @@
+package com.furnifit.member.service;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import com.furnifit.member.dao.CouponDao;
+import com.furnifit.member.domain.Coupon;
+
+@Service
+public class CouponServiceImpl implements CouponService {
+	
+	@Inject
+	private CouponDao daodao;
+/*	
+	@Override
+	public List<Coupon> read(String email) throws Exception {
+		return daodao.read(email);
+	}
+*/
+	
+	@Override
+	public List<Coupon> read(String email) throws Exception {
+		return daodao.read(email);
+	}
+	
+}
