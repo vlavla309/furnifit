b0e66e6da733c674fa61409991fd77b1147ce641
sontom <illusioney@naver.com>
2017-11-16 12:27:43 +0900

    planItem컨트롤러 수정

--- Furnifit/src/main/java/com/furnifit/planitem/controller/PlanItemController.java
+++ Furnifit/src/main/java/com/furnifit/planitem/controller/PlanItemController.java
@@ -23,64 +23,13 @@ import com.furnifit.product.service.ProductService;
 
 
 /**
- * OrdersController 클래스
+ * PlanItemController 클래스
  * @author 손정화
  *
  */
 @Controller
-@RequestMapping("/mypage/order")
+@RequestMapping("/mypage/itemlist")
 public class PlanItemController {
 	
-	Logger logger = Logger.getLogger(PlanItemController.class);
-	
-	@Inject
-	private OrdersService ordersService;
-	@Inject
-	private ProductService proService;
-	@Inject
-	private OrderitemsService itemsService;
-	
-	
-	/** 주문내역 목록  */
-	@RequestMapping(value = "", method=RequestMethod.GET)
-	public String listAll(Model model, HttpServletRequest request) throws Exception {
-		
-		HttpSession session = request.getSession();   
-		Member member = (Member) session.getAttribute("login");
-		
-		List<Orders> orderList = ordersService.listAll(member.getEmail());
-		for (Orders orders : orderList) {
-			logger.info(orders);
-		}
-		
-		List<Product> proList = proService.list();
-		for (Product product : proList) {
-			logger.info(product);
-		}
-		
-		model.addAttribute("orderlist", orderList);
-		model.addAttribute("member", member);
-		model.addAttribute("prolist", proList);
-		return "order/order-list";
-	}
-	
-	
-	/** 주문내역 상세보기  */
-	@RequestMapping(value = "/{orderId}", method=RequestMethod.GET)
-	public String read(@PathVariable("orderId") int orderId, Model model, HttpServletRequest request) throws Exception {
-		
-		HttpSession session = request.getSession();   
-		Member member = (Member) session.getAttribute("login");
-		
-		model.addAttribute(ordersService.read(orderId));
-		
-		List<Orderitems> list = itemsService.listAll(member.getEmail());
-		for (Orderitems orderitems : list) {
-			logger.info(orderitems);
-		}
-		
-		model.addAttribute("list", list);
-		return "order/order-info";
-	}
 
 }
