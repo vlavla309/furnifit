d791bb3f3bd55692e7993de2228031050e0f979b
sontom <illusioney@naver.com>
2017-11-16 18:40:17 +0900

    plan 커밋

---
+++ Furnifit/src/main/java/com/furnifit/plan/service/PlanServiceImpl.java
@@ -0,0 +1,35 @@
+package com.furnifit.plan.service;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import com.furnifit.plan.dao.PlanDao;
+import com.furnifit.plan.domain.Plan;
+import com.furnifit.planitem.dao.PlanItemDao;
+import com.furnifit.planitem.domain.PlanItem;
+
+/**
+ * PlanItemServiceImpl 클래스
+ * @author 손정화
+ *
+ */
+@Service
+public class PlanServiceImpl implements PlanService {
+	
+	@Inject
+	private PlanDao dao;
+
+	@Override
+	public List<Plan> listAll(String email) throws Exception {
+		return dao.listAll(email);
+	}
+	
+	@Override
+	public Plan read(int planitemId) throws Exception {
+		return dao.read(planitemId);
+	}
+	
+}
