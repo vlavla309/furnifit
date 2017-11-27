90f5936e678236abf7220b82bc6e29184d818cc1
furnifit <dinozo12112@gmail.com>
2017-11-27 18:14:44 +0900

    brand 컨트롤러

---
+++ Furnifit/src/main/java/com/furnifit/brand/controller/BrandController.java
@@ -0,0 +1,13 @@
+package com.furnifit.brand.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/brand")
+public class BrandController {
+	private static final Logger logger = Logger.getLogger(BrandController.class);
+
+}
