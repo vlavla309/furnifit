08b0d5489f18a374b42f292ed92d5653039f302c
kguong123 <kguong123@naver.com>
2017-11-27 12:36:08 +0900

    ㄹㄹㄹ

--- Furnifit/src/test/java/com/furnifit/member/test/CouponDaoTest.java
+++ Furnifit/src/test/java/com/furnifit/member/test/CouponDaoTest.java
@@ -12,8 +12,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.furnifit.member.dao.CouponDao;
-import com.furnifit.member.domain.Coupon;
+import com.furnifit.coupon.dao.CouponDao;
+import com.furnifit.coupon.domain.Coupon;
+
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/*.xml" }) 
