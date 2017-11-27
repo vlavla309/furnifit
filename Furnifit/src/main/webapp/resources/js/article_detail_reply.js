f90e3b12d5d60b1957cdf1bec3569af9b0086526
furnifit <dinozo12112@gmail.com>
2017-11-27 11:56:16 +0900

    커밋

--- Furnifit/src/main/webapp/resources/js/article_detail_reply.js
+++ Furnifit/src/main/webapp/resources/js/article_detail_reply.js
@@ -1,3 +1,4 @@
+
 /* 댓글들이 처음 출력되는 코드 */
 var articleId = $("#newArticleId").val();
 var email = $("#newEmail").val();
@@ -12,13 +13,13 @@ $.getJSON("/one/reply/all/" + articleId, function(data) {
   $(data).each(
       function() {
         str += "<li class='replyLi' data-rno=" + this.reply_id + ">"
-        	/* + "<data-rno="+this.reply_id+"class='replyLi'>" */
-        	+"<i class='fa fa-comments bg-blue'></i>"
-        	+"<div class='timeline-time'>"
-        	+"<span class='time'>"
-        	+"<span class='fa fa-clock-o' style='margin-left:84%'>" + " " + "<span style='font-size:12px';>" + this.regdate + "</span>" + "</span>"
-        	+"</span>"
-        	+"<div class='timeline-header' style='font-size: 14px;'>" + this.email + "</div>"
+          /* + "<data-rno="+this.reply_id+"class='replyLi'>" */
+          +"<i class='fa fa-comments bg-blue'></i>"
+          +"<div class='timeline-time'>"
+          +"<span class='time'>"
+          +"<span class='fa fa-clock-o' style='margin-left:84%'>" + " " + "<span style='font-size:12px';>" + this.regdate + "</span>" + "</span>"
+          +"</span>"
+          +"<div class='timeline-header' style='font-size: 14px;'>" + this.email + "</div>"
             + "<br>" + "<div class='timeline-body'><strong>" + this.content + "</strong>" + "</div>" + "<br><br>"
             + "<div class='timeline-footer'>" + "<a class='btn btn-danger btn-xs' data-toggle='modal' data-target='#modifyModal'>Modify</a>"
             + "</div>"
@@ -30,13 +31,15 @@ $.getJSON("/one/reply/all/" + articleId, function(data) {
 });
 getList();
 
+/* var articleId = $("#newArticleId").val();
+var email = $("#newEmail").val();  */
+
 /* 무한 스크롤링 코드 */
 $(window).scroll(function() {
     console.log($(window).scrollTop());
     console.log($(window).height());
     console.log($(document).height());
-	
-    
+   
     var scrollHeight = $(window).scrollTop() + $(window).height();
     var documentHeight = $(document).height();
     console.log(scrollHeight + ":" + documentHeight);
@@ -45,9 +48,9 @@ $(window).scroll(function() {
     // document 높이와 같아진다.
     if (scrollHeight >= documentHeight - 100) { //브라우저 마다 미세한 차이 발생으로 -100
 
-    	var reply_id = $(".replyLi:last").attr("data-rno");
-    	/* var articleId = ${article.articleId}; */
-
+      var reply_id = $(".replyLi:last").attr("data-rno");
+      /* var articleId = ${article.articleId}; */
+	  
         $.ajax({
             url: "/one/reply/infiniteScrollDown",
             data: {
@@ -56,18 +59,18 @@ $(window).scroll(function() {
             },
             dataType: "json",
             success: function(data) {
-            	console.log(data);
+              console.log(data);
                 var output = "";
                 $(data).each(
-                	      function() {
+                        function() {
                     output += "<li class='replyLi' data-rno=" + this.reply_id + ">"
-                	/* + "<data-rno="+this.reply_id+"class='replyLi'>" */
-                	+"<i class='fa fa-comments bg-blue'></i>"
-                	+"<div class='timeline-time'>"
-                	+"<span class='time'>"
-                	+"<span class='fa fa-clock-o' style='margin-left:84%'>" + " " + "<span style='font-size:12px';>" + this.regdate + "</span>" + "</span>"
-                	+"</span>"
-                	+"<div class='timeline-header' style='font-size: 14px;'>" + this.email + "</div>"
+                  /* + "<data-rno="+this.reply_id+"class='replyLi'>" */
+                  +"<i class='fa fa-comments bg-blue'></i>"
+                  +"<div class='timeline-time'>"
+                  +"<span class='time'>"
+                  +"<span class='fa fa-clock-o' style='margin-left:84%'>" + " " + "<span style='font-size:12px';>" + this.regdate + "</span>" + "</span>"
+                  +"</span>"
+                  +"<div class='timeline-header' style='font-size: 14px;'>" + this.email + "</div>"
                     /* + this.reply_id + ":" + this.content + ":" + this.email + ":" + this.regdate + "<br>" */
                     /* + "<button>MOD</button></li>";  */
                     /* + "<span class='timeline-body'></span>" */
@@ -85,9 +88,9 @@ $(window).scroll(function() {
 
 // 댓글을 등록하는 코드
 $("#replyAddBtn").on("click", function() {
-	/* var email = $("#newEmail").val(); */
-	var content = $("#newContent").val();
-	/* var articleId  = $("#newArticleId").val(); */
+  /* var email = $("#newEmail").val(); */
+  var content = $("#newContent").val();
+  /* var articleId  = $("#newArticleId").val(); */
 
   $.ajax({
     type : 'post',
@@ -97,10 +100,10 @@ $("#replyAddBtn").on("click", function() {
       "X-HTTP-Method-Override" : "POST"},
     dataType : 'text',
     data : JSON.stringify({
-    	email : email,
-    	content : content,
-    	articleId : articleId,
-    	}),
+      email : email,
+      content : content,
+      articleId : articleId,
+      }),
     success : function(result) {
       console.log("result: " + result);
       if (result == 'SUCCESS') {
@@ -114,12 +117,12 @@ $("#replyAddBtn").on("click", function() {
 
 // 댓글 수정, 삭제할 때 데이터를 모달에 제공한다.
 $("#replies").on("click", ".replyLi", function(event){
-	
-	var reply = $(this);
-	
-	$("#content").val(reply.find('.timeline-body').text());
-	$(".modal-title").html(reply.attr("data-rno"));
-	console.log(content)	
+  
+  var reply = $(this);
+  
+  $("#content").val(reply.find('.timeline-body').text());
+  $(".modal-title").html(reply.attr("data-rno"));
+  console.log(content)  
 });
 
 // 댓글을 삭제하는 코드
@@ -129,10 +132,10 @@ $("#replyDelBtn").on("click", function() {
   var content = $("#content").val();
 
   var RegexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;  // 유효성 검사이다. 이메일을 이용하여 회원만 삭제하도록 한다.                                                    
-																		//#newEmail이 비어 있는지 안 비어있는지로 확인할 수도 있지만 나중에 비회원도 댓글을
-  if ( !RegexEmail.test($.trim($("#newEmail").val())) ) { 				// 달 수 있게 변경될 수도 있어서 위와 같이 했다.              
+                                    //#newEmail이 비어 있는지 안 비어있는지로 확인할 수도 있지만 나중에 비회원도 댓글을
+  if ( !RegexEmail.test($.trim($("#newEmail").val())) ) {         // 달 수 있게 변경될 수도 있어서 위와 같이 했다.              
         alert("로그인 후 이용해주시기 바랍니다.");                      
-  		return false;
+      return false;
   } else {
     $.ajax({
       type : 'delete',
@@ -161,11 +164,11 @@ $("#replyModBtn").on("click",function(){
     var content = $("#content").val();
     
     var RegexEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;  // 유효성 검사이다. 이메일을 이용하여 회원만 수정하도록 한다.
-  																		  //#newEmail이 비어 있는지 안 비어있는지로 확인할 수도 있지만 나중에 비회원도 댓글을	
-    if ( !RegexEmail.test($.trim($("#newEmail").val())) ) { 			  // 달 수 있게 변경될 수도 있어서 위와 같이 했다.
-		alert("로그인 후 이용해주시기 바랍니다.");
-		return false;
-	} else {
+                                        //#newEmail이 비어 있는지 안 비어있는지로 확인할 수도 있지만 나중에 비회원도 댓글을  
+    if ( !RegexEmail.test($.trim($("#newEmail").val())) ) {         // 달 수 있게 변경될 수도 있어서 위와 같이 했다.
+    alert("로그인 후 이용해주시기 바랍니다.");
+    return false;
+  } else {
       $.ajax({
         type:'put',
         url:'/one/reply/'+ reply_id,
@@ -182,7 +185,5 @@ $("#replyModBtn").on("click",function(){
             getList();
           }
       }});
-	}
+  }
 });   
-         
-          
