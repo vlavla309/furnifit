bd5044344966293cd62fb52441050b9956c4969c
khj <khj309@naver.com>
2017-11-29 14:00:20 +0900

    가구 배치도 이미지 및 에디터 수정

--- Furnifit/src/main/webapp/resources/js/plan/converter.js
+++ Furnifit/src/main/webapp/resources/js/plan/converter.js
@@ -1,3 +1,31 @@
 /**
- * 
+ * 이미지 경로를 입력받아 해당 이미지를 base64로 인코딩
  */
+
+var converterEngine = function (input) { // fn BLOB => Binary => Base64 ?
+    var uInt8Array = new Uint8Array(input),
+        i = uInt8Array.length;
+    var biStr = []; //new Array(i);
+    while (i--) {
+        biStr[i] = String.fromCharCode(uInt8Array[i]);
+    }
+    var base64 = window.btoa(biStr.join(''));
+    //console.log("2. base64 produced >>> " + base64); // print-check conversion result
+    return base64;
+};
+
+var getImageBase64 = function (url, callback) {
+    // 1. Loading file from url:
+    var xhr = new XMLHttpRequest(url);
+    xhr.open('GET', url, true); // url is the url of a PNG image.
+    xhr.responseType = 'arraybuffer';
+    xhr.callback = callback;
+    xhr.onload = function (e) {
+        if (this.status == 200) { // 2. When loaded, do:
+            //console.log("1:Loaded response >>> " + this.response); // print-check xhr response 
+            var imgBase64 = converterEngine(this.response); // convert BLOB to base64
+            this.callback(imgBase64); //execute callback function with data
+        }
+    };
+    xhr.send();
+};
